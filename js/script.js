4c02c40e1c65a1dbce317a2e02dcfa6f455bfced
Guido <ppapaguido@gmail.com>
2021-11-05 14:56:33 +0100

    aggiunta di un minimo di css

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-const eta = parseInt(prompt('inserisci eta'));
+const eta = parseInt(prompt('Inserisci eta'));
 console.log('eta scritta ' + eta);
-document.getElementById('eta').innerHTML ='l eta della persona e: ' + eta + ' anni';
+document.getElementById('eta').innerHTML ='L eta della persona e: ' + eta + ' anni';
 
-let dist = parseFloat (prompt('inserisci quanti KM farai'));
+let dist = parseFloat (prompt('Inserisci quanti KM farai'));
 console.log('distanza scritta ' + dist);
-document.getElementById('distanza').innerHTML ='la distanza totale del viaggio e: ' + dist + ' KM';
+document.getElementById('distanza').innerHTML ='La distanza totale del viaggio e: ' + dist + ' KM';
 
 
 let prezzo = dist * 0.21;
 
 if (eta < 18){
     let sconto = prezzo - prezzo * 20 / 100;
-    document.getElementById('costo').innerHTML ='il prezzo del biglietto e di: ' + sconto.toFixed(2);
+    document.getElementById('costo').innerHTML ='Il prezzo del biglietto e di: ' + sconto.toFixed(2);
     console.log('il biglietto costa ' + sconto.toFixed(2));
 }
 else if (eta >= 65) {
     let sconto = prezzo - prezzo * 40 /100;
-    document.getElementById('costo').innerHTML ='il prezzo del biglietto e di: ' + sconto.toFixed(2);
+    document.getElementById('costo').innerHTML ='Il prezzo del biglietto e di: ' + sconto.toFixed(2);
     console.log('il biglietto costa ' + sconto.toFixed(2));
 }
 else {
-    document.getElementById('costo').innerHTML ='il prezzo del biglietto e di: ' + prezzo.toFixed(2);
+    document.getElementById('costo').innerHTML ='Il prezzo del biglietto e di: ' + prezzo.toFixed(2);
     console.log('il biglietto costa ' + prezzo.toFixed(2))
 }
